d07ae5e7c89daa1b04127e88dee8891f47d116b2
Nancy <butlern0513@gmail.com>
2018-12-06 15:19:35 -0700

    nailed it

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,27 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+app.get('/hello', function(req, res) {
+  res.send("Hello!");
 });
+
+app.post('/create/:name', (req,res)=>{
+  res.json({"id": 1, "name":req.params.name})
+})
+
+app.get('/', (req,res)=>{
+  res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get('/verify/:age',(req,res)=>{
+  if(req.params.age >=13){
+    res.sendStatus(200)
+  }
+  else{
+    res.sendStatus(403)
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+const fs = require('fs')
 var port = process.env.PORT || 8000;
+let idCount = 1;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/create/:name/:age', function(req, res) {
+ let olddata = JSON.parse(fs.readFileSync(`${__dirname}/storage.json`))
+ olddata.push({
+   id: idCount,
+   name: req.params.name,
+   age: req.params.age
+ })
+ fs.writeFileSync(`${__dirname}/storage.json`, JSON.stringify(olddata))
+ idCount++
+ res.end()
+  // fs.appendFileSync(`${__dirname}/storage.json`,JSON.stringify(new Person(req.params.name, req.params.age)))
 });
+
+app.get('/', (req,res)=>{
+  res.type('json');
+  res.send(fs.readFileSync(`${__dirname}/storage.json`))
+})
+
+app.get('/:id',(req,res)=>{
+  let names = require('./storage.json');
+  res.json(names.filter((person)=>person.id == req.params.id))
+  res.end()
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
